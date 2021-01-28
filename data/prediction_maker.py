f02e56305c76fc586ed1af582fd79e89dc1403ac
Hannah Mehravari <hannahmehravari@gmail.com>
2021-01-28 20:40:19 +0000

    Create prediction_maker.py

---
+++ data/prediction_maker.py
@@ -0,0 +1,13 @@
+class PredictionMaker:
+    def __init__(self, time_step=1, p=1, q=1, d=0):
+        self.time_step = time_step
+        self.p = p
+        self.q = q
+        self.d = d
+
+    def make_prediction(training_data):
+        model = ARIMA(training_data, order=(self.p, self.q, self.d))
+        model_fit = model.fit(disp=0)
+        output = model_fit.forecast(self.time_step)
+        yhat = output[0].flatten()[0]
+        return yhat
