3f176b7a42d64ff5d9041f6ee3d6e9fdfcb1c55d
Hannah Mehravari <hannahmehravari@gmail.com>
2021-02-09 14:40:33 +0000

    Fix histogram.py

--- noise_campaign/histogram.py
+++ noise_campaign/histogram.py
@@ -5,42 +5,47 @@ class Histogram:
         self,
         wind_speed_min: int,
         wind_speed_max: int,
-        wind_speed_bin_count: int,
+        wind_speed_bin_size: int,
         wind_direction_min: int,
         wind_direction_max: int,
-        wind_direction_bin_count: int):
-
-        wind_speed_bin_size = (wind_speed_max - wind_speed_min) / float(wind_speed_bin_count)
-        wind_direction_bin_size = (wind_direction_max - wind_direction_min) / float(wind_direction_bin_count)
+        wind_direction_bin_size: int):
 
         self.bins = []
-        for x in range(0, wind_speed_bin_count):
-            for y in range(0, wind_direction_bin_count):
-            wind_speed_bin_start = wind_speed_min + x * wind_speed_bin_size
-            wind_speed_bin_end = wind_speed_bin_start + wind_speed_bin_size
-            wind_direction_bin_start = wind_direction_min + y * wind_direction_bin_size
-            wind_direction_bin_end = wind_direction_bin_start + wind_direction_bin_count
+        for x in range(wind_speed_min,wind_speed_max, wind_speed_bin_size):
+            for y in range(wind_direction_min, wind_direction_max, wind_direction_bin_size):
+                wind_speed_bin_start = x
+                wind_speed_bin_end = wind_speed_bin_start + wind_speed_bin_size
+                wind_direction_bin_start = y 
+                wind_direction_bin_end = wind_direction_bin_start + wind_direction_bin_size
 
-            new_bin = HistogramBin(
-                wind_speed_bin_start,
-                wind_speed_bin_end,
-                wind_direction_bin_start,
-                wind_direction_bin_end)
+                new_bin = HistogramBin(
+                    wind_speed_bin_start,
+                    wind_speed_bin_end,
+                    wind_direction_bin_start,
+                    wind_direction_bin_end)
 
-            self.bins.append(new_bin)
+                self.bins.append(new_bin)
 
-    
+    def get_bins_list(self):
+        return self.bins
     def get_incomplete_bins(self):
         incomplete_bins = []
-        for histogram_bin in bins:
+        for histogram_bin in self.bins:
             if histogram_bin.has_missing_measurements:
                 incomplete_bins.append(histogram_bin)
         return incomplete_bins
 
     
-    def insert_measurement(self, wind_speed, wind_direction, turbine_status, noise_measurement):
-        for histogram_bin in bins:
+    def get_correct_bin(self, wind_speed, wind_direction):
+        for histogram_bin in self.bins:
             if histogram_bin.can_store_measurement(wind_speed, wind_direction):
-                histogram_bin.set_measurement(turbine_status, noise_measurement)
+                return histogram_bin
 
+
+if __name__ == '__main__':
+    h = Histogram(0, 1, 1, 0, 1, 1)
+    #b = h.get_correct_bin(11,120)
+    #print(b.wind_speed_range, b.wind_direction_range)
+    for b in h.get_bins_list():
+        print(b.wind_speed_range, b.wind_direction_range)
 
--- noise_campaign/histogram_bin.py
+++ noise_campaign/histogram_bin.py
@@ -1,3 +1,5 @@
+from noise_campaign.turbine_status import TurbineStatus
+
 class HistogramBin:
     def __init__(
         self,
@@ -6,32 +8,11 @@ class HistogramBin:
         bin_wind_direction_start,
         bin_wind_direction_end):
 
-        self.wind_speed_range = range(bin_wind_speed_start, bin_wind_speed_end)
-        self.wind_direction_range = range(bin_wind_direction_start, bin_wind_direction_end)
+        self.wind_speed_range = range(int(bin_wind_speed_start), int(bin_wind_speed_end))
+        self.wind_direction_range = range(int(bin_wind_direction_start), int(bin_wind_direction_end))
 
         self.measurement_with_turbine_running = None
         self.measurement_with_turbine_paused = None
-
-    def has_missing_running_measurement(self):
-        return self.measurement_with_turbine_running is None
-
-    def has_missing_paused_measurement(self):
-        return self.measurement_with_turbine_paused is None
-
-    def has_missing_measurements(self):
-        if self.has_missing_paused_measurement or self.has_missing_running_measurement:
-            return True 
-        return False
-
-    def set_measurement(
-        self,
-        turbine_status: TurbineStatus,
-        noise_measurement: float)
-
-        if turbine_status == TurbineStatus.RUNNING:
-            self.measurement_with_turbine_running = noise_measurement
-        elif turbine_status == TurbineStatus.PAUSED:
-            self.measurement_with_turbine_paused = noise_measurement
 
     def can_store_measurement(
         self,
