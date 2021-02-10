e594afe6b67b0ae85c7907d6d761db26d1bac3ba
Hannah Mehravari <hannahmehravari@gmail.com>
2021-02-10 21:21:05 +0000

    Change Histogram bins to match RES requirements

--- api/noise_campaign/histogram.py
+++ api/noise_campaign/histogram.py
@@ -1,51 +1,42 @@
-from noise_campaign.histogram_bin import HistogramBin
+import numpy as np
 
 class Histogram:
-    def __init__(
-        self,
-        wind_speed_min: int,
-        wind_speed_max: int,
-        wind_speed_bin_size: int,
-        wind_direction_min: int,
-        wind_direction_max: int,
-        wind_direction_bin_size: int):
+    def __init__(self):
+        pass
 
-        self.bins = []
-        for x in range(wind_speed_min,wind_speed_max, wind_speed_bin_size):
-            for y in range(wind_direction_min, wind_direction_max, wind_direction_bin_size):
-                wind_speed_bin_start = x
-                wind_speed_bin_end = wind_speed_bin_start + wind_speed_bin_size
-                wind_direction_bin_start = y 
-                wind_direction_bin_end = wind_direction_bin_start + wind_direction_bin_size
+    def get_wind_speed_bins(self, center_of_first_bin, number_of_bins, bin_width):
+        center_of_last_bin = (
+            number_of_bins * bin_width - bin_width + center_of_first_bin
+        )
+        bin_centers = np.arange(
+            start=center_of_first_bin,
+            stop=center_of_last_bin + bin_width,
+            step=bin_width,
+        )
+        bins = {}
+        for center in bin_centers:
+            bins[center] = self._create_bin_interval(center, bin_width)
+        return bins
 
-                new_bin = HistogramBin(
-                    wind_speed_bin_start,
-                    wind_speed_bin_end,
-                    wind_direction_bin_start,
-                    wind_direction_bin_end)
+    def get_wind_direction_bins(self, number_of_bins):
+        bin_width = 360.0 / number_of_bins
+        bin_centers = np.arange(0.0, 360.0, bin_width)
+        bin_edges = np.arange(start=0.0 - bin_width / 2, stop=360.0, step=bin_width)
+        bins = {}
+        for bin_center in bin_centers:
+            bins[bin_center] = self._create_bin_interval(bin_center, bin_width)
+        return bins
 
-                self.bins.append(new_bin)
-
-    def get_bins_list(self):
-        return self.bins
-    def get_incomplete_bins(self):
-        incomplete_bins = []
-        for histogram_bin in self.bins:
-            if histogram_bin.has_missing_measurements:
-                incomplete_bins.append(histogram_bin)
-        return incomplete_bins
-
-    
-    def get_correct_bin(self, wind_speed, wind_direction):
-        for histogram_bin in self.bins:
-            if histogram_bin.can_store_measurement(wind_speed, wind_direction):
-                return histogram_bin
+    def _create_bin_interval(self, bin_center, bin_width):
+        lower_bin_limit = bin_center - (bin_width / 2.0)
+        upper_bin_limit = bin_center + (bin_width / 2.0)
+        return [lower_bin_limit, upper_bin_limit]
 
 
-if __name__ == '__main__':
-    h = Histogram(0, 1, 1, 0, 1, 1)
-    #b = h.get_correct_bin(11,120)
-    #print(b.wind_speed_range, b.wind_direction_range)
-    for b in h.get_bins_list():
-        print(b.wind_speed_range, b.wind_direction_range)
+if __name__ == "__main__":
+    h = Histogram()
+    # i = h.get_wind_speed_bins(0, 2, 1)
+    # print(i)
+    i = h.get_wind_direction_bins(12)
+    print(i)
 
--- api/test/test_histogram.py
+++ api/test/test_histogram.py
@@ -1,18 +1,28 @@
 from noise_campaign.histogram import Histogram
-from noise_campaign.histogram_bin import HistogramBin
+histogram = Histogram()
+
 def test_histogram_bins_creation_speed():
-    histogram = Histogram(0, 1, 1, 0, 1, 1)
+    histogram = Histogram()
+    actual_bins = histogram.get_wind_speed_bins(0, 2, 1)
+    expected_bins = {0: [-0.5, 0.5], 1: [0.5, 1.5]}
+    assert expected_bins == actual_bins
 
-    actual_bins = histogram.get_bins_list()
-    expected_bins = [HistogramBin(0,1,0,1)]
 
-    assert expected_bins[0].wind_speed_range == actual_bins[0].wind_speed_range
-    
 def test_histogram_bins_creation_direction():
-    histogram = Histogram(0, 1, 1, 0, 1, 1)
+    actual_bins = histogram.get_wind_direction_bins(12)
+    expected_bins = {
+        0.0: [-15.0, 15.0],
+        30.0: [15.0, 45.0],
+        60.0: [45.0, 75.0],
+        90.0: [75.0, 105.0],
+        120.0: [105.0, 135.0],
+        150.0: [135.0, 165.0],
+        180.0: [165.0, 195.0],
+        210.0: [195.0, 225.0],
+        240.0: [225.0, 255.0],
+        270.0: [255.0, 285.0],
+        300.0: [285.0, 315.0],
+        330.0: [315.0, 345.0],
+    }
 
-    actual_bins = histogram.get_bins_list()
-    expected_bins = [HistogramBin(0,1,0,1)]
-
-    assert expected_bins[0].wind_direction_range == actual_bins[0].wind_direction_range
-    
+    assert expected_bins == actual_bins
