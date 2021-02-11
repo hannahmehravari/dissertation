91b3790e8e2fb06eabf9fb99eda40fe16684d30e
Hannah Mehravari <hannahmehravari@gmail.com>
2021-02-11 19:07:17 +0000

    Add tear down and build up script

---
+++ nuke.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker-compose down
+docker container prune -f
+docker volume prune -f
+docker image prune -f
+docker volume create --name=chronograf-storage
+docker volume create --name=influxdb-storage
+docker-compose up --build
+
