e5377aa7123e77cffe080b011f27394909f0b2f6
Hannah Mehravari <hannahmehravari@gmail.com>
2021-05-28 13:02:22 +0100

    Update measured_state.py

--- api/noise_campaign/measured_state.py
+++ api/noise_campaign/measured_state.py
@@ -1,12 +1,17 @@
 import json
 from noise_campaign.smartstop_measurement_names import SMARTStopMeasurementNames
+from noise_campaign.histogram import Histogram
 from statistics import mean
 from datetime import datetime
 class MeasuredState:
-    def __init__(self, request_json: dict):
+    def __init__(self, histogram: Histogram, request_json: dict):
         data_dict = request_json
         self.average_wind_speed = self._extract_average_measurement(data_dict, SMARTStopMeasurementNames.wind_speed)
         self.average_wind_direction = self._extract_average_measurement(data_dict, SMARTStopMeasurementNames.wind_direction)
+
+        self.speed_bin = histogram.get_wind_speed_bin(self.average_wind_speed)
+        self.direction_bin = histogram.get_wind_direction_bin(self.average_wind_direction)
+
         self.timestamp = self._get_time_stamp(data_dict)
 
     def _get_time_stamp(self, data_dict):
