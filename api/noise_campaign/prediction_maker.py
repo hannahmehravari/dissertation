f1a7d3e8a88be0e1ff41801fd78b1c7c8b65fed3
Hannah Mehravari <hannahmehravari@gmail.com>
2021-06-24 20:02:31 +0100

    Update prediction_maker.py

--- api/noise_campaign/prediction_maker.py
+++ api/noise_campaign/prediction_maker.py
@@ -1,14 +1,14 @@
 from statsmodels.tsa.arima_model import ARIMA
 
 class PredictionMaker:
-    def __init__(self, time_step=1, p=1, q=1, d=0):
+    def __init__(self, time_step=1, p=1, q=0, d=1):
         self.time_step = time_step
         self.p = p
         self.q = q
         self.d = d 
 
-    def make_prediction(training_data):
-        model = ARIMA(training_data, order=(self.p, self.q, self.d))
+    def make_prediction(self, training_data):
+        model = ARIMA(training_data, order=(self.p, self.d, self.q))
         model_fit = model.fit(disp=0)
         output = model_fit.forecast(self.time_step)
         yhat = output[0].flatten()[0]
