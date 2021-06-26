cce701b034816ce04130a9af4f492b458e98000f
Hannah Mehravari <hannahmehravari@gmail.com>
2021-06-26 23:43:12 +0100

    Update data_handler.py

--- api/noise_campaign/data_handler.py
+++ api/noise_campaign/data_handler.py
@@ -2,6 +2,7 @@ from noise_campaign.measured_state import MeasuredState
 from noise_campaign.predicted_state import PredictedState
 import pandas as pd
 import datetime as dt
+import pytz
 
 
 class DataHandler:
@@ -9,14 +10,17 @@ class DataHandler:
         self.db_client = db_client
 
     def write_measured_state(self, measured_state: MeasuredState):
-        self.db_client.switch_database("30s_observed_data")
+        self.db_client.switch_database("noise_campaign")
         point_dict = {
             measured_state.timestamp: [
                 measured_state.average_wind_speed,
                 measured_state.average_wind_direction,
+                measured_state.wind_speed_bin,
+                measured_state.wind_direction_bin,
+                measured_state.status
             ]
         }
-        columns = ["wind_speed", "wind_direction"]
+        columns = ["wind_speed", "wind_direction", "speed_bin", "direction_bin", "turbine_state"]
         point_df = pd.DataFrame.from_dict(
             data=point_dict, orient="index", columns=columns
         )
@@ -34,20 +38,19 @@ class DataHandler:
 
     def write_predicted_state(self, predicted_state: PredictedState):
 
-        self.db_client.switch_database("predicted_data")
+        self.db_client.switch_database("noise_campaign")
 
         point_dict = {
             predicted_state.timestamp: [
                 predicted_state.average_wind_speed,
                 predicted_state.average_wind_direction,
+                predicted_state.wind_speed_bin,
+                predicted_state.wind_direction_bin,
+                predicted_state.turbine_status,
             ]
         }
 
-        columns = [
-            "wind_speed",
-            "wind_direction",
-        ]
-
+        columns = ["wind_speed", "wind_direction", "speed_bin", "direction_bin", "turbine_state"]
         point_df = pd.DataFrame.from_dict(
             data=point_dict, orient="index", columns=columns
         )
@@ -63,14 +66,15 @@ class DataHandler:
 
 
     def write_aggregated_state(self, wind_speed, wind_direction, wind_speed_bin, wind_direction_bin, turbine_status, timestamp):
-        self.db_client.switch_database("10min_observed_data")
+        self.db_client.switch_database("noise_campaign")
         point_dict = {
             timestamp: [
                 wind_speed,
                 wind_direction,
+                wind_speed_bin, wind_direction_bin, turbine_status,
             ]
         }
-        columns = ["wind_speed", "wind_direction"]
+        columns = ["wind_speed", "wind_direction", "speed_bin", "direction_bin", "turbine_state"]
         point_df = pd.DataFrame.from_dict(
             data=point_dict, orient="index", columns=columns
         )
@@ -90,23 +94,22 @@ class DataHandler:
         return None
 
     def get_training_set(self, training_set_size):
-        print(training_set_size)
-        self.db_client.switch_database("10min_observed_data")
+        self.db_client.switch_database("noise_campaign")
         result = self.db_client.query(
             f'SELECT * FROM "10min_sample" ORDER BY desc LIMIT {training_set_size}'
         )
         return result['10min_sample']
 
-    def get_latest_samples(self, size):
-        self.db_client.switch_database("30s_observed_data")
+    def get_latest_samples(self):
+        self.db_client.switch_database("noise_campaign")
         result = self.db_client.query(
-            f'SELECT * FROM "30s_sample" ORDER BY desc LIMIT {int(size)}'
+            f'SELECT * FROM "30s_sample"'
         )
 
         return result['30s_sample']
 
     def get_10min_observed_data_size(self):
-        self.db_client.switch_database("10min_observed_data")
+        self.db_client.switch_database("noise_campaign")
         result = self.db_client.query(
             'SELECT COUNT("wind_speed") FROM "10min_sample"'
         )
@@ -114,7 +117,7 @@ class DataHandler:
         return result['10min_sample']['count'][0]
 
     def get_30s_observed_data_size(self):
-        self.db_client.switch_database("30s_observed_data")
+        self.db_client.switch_database("noise_campaign")
         result = self.db_client.query(
             'SELECT COUNT("wind_speed") FROM "30s_sample"'
         )
@@ -122,9 +125,8 @@ class DataHandler:
         return result['30s_sample']['count'][0]
 
     def get_number_of_measurements_by_bin_and_status(self, speed_bin, direction_bin, status):
-        self.db_client.switch_database("10min_observed_data")
+        self.db_client.switch_database("noise_campaign")
         result = self.db_client.query(f"SELECT COUNT(wind_speed) FROM \"10min_sample\" WHERE wind_speed_bin=\'{speed_bin}\' AND wind_direction_bin=\'{direction_bin}\' AND run_state=\'{status}\'")
-        print(result)
 
         if len(result) == 0:
             return 0
@@ -132,5 +134,28 @@ class DataHandler:
             return result['10min_sample']['count'][0]
 
     def clear_30s_measurements(self):
-        self.db_client.switch_database("30s_observed_data")
+        self.db_client.switch_database("noise_campaign")
         self.db_client.query('DROP SERIES FROM "30s_sample"')
+
+    def get_last_timestamp_in_30_second_measurement(self):
+
+        self.db_client.switch_database("noise_campaign")
+        result = self.db_client.query('SELECT * FROM "30s_sample" ORDER BY time DESC LIMIT 1')
+
+        if len(result) == 0:
+            return 0
+        else:
+            return result['30s_sample'].index[0]
+    
+
+    def get_last_timestamp_in_10_minute_measurement(self):
+
+        self.db_client.switch_database("noise_campaign")
+        result = self.db_client.query('SELECT * FROM "10min_sample" ORDER BY time DESC LIMIT 1')
+
+        if len(result) == 0:
+            return None
+        else:
+            return result['10min_sample'].index[0]
+
+
