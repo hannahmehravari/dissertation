377245e4de4a677187486f73b5e9b7b4e28ef07e
Hannah Mehravari <hannahmehravari@gmail.com>
2021-06-26 23:43:16 +0100

    Update init_database.py

--- api/noise_campaign/init_database.py
+++ api/noise_campaign/init_database.py
@@ -5,15 +5,13 @@ import os
 def init_db():
     db_client = DataFrameClient(host=os.getenv("DB_HOST"), port=os.getenv("DB_PORT"))
 
-    db_client.create_database("30s_observed_data")
-    db_client.create_retention_policy("2_weeks", "14d", 1, "30s_observed_data", default=True)
+    try:
+        db_client.query("DROP DATABASE \"noise_campaign\"")
+    except:
+        pass
 
-    db_client.create_database("10min_observed_data")
-    db_client.create_retention_policy("2_weeks", "14d", 1, "10min_observed_data", default=True)
-
-    db_client.create_database("predicted_data")
-    db_client.create_retention_policy("2_weeks", "14d", 1, "predicted_data", default=True)
-
+    db_client.create_database("noise_campaign")
+    # db_client.create_retention_policy("2_weeks", "14d", 1, "noise_campaign", default=True)
 
     return db_client
 
